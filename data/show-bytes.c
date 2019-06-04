3f8cb3510a6795e48c2ea3faea5474661cde25df
alexhuangster <alxddhuang@gmail.com>
2019-06-04 11:09:53 +0800

    modify indent

--- data/show-bytes.c
+++ data/show-bytes.c
@@ -10,7 +10,7 @@ typedef unsigned char *byte_pointer;
 void show_bytes(byte_pointer start, size_t len) {
     size_t i;
     for (i = 0; i < len; i++)
-	printf(" %.2x", start[i]);    //line:data:show_bytes_printf
+        printf(" %.2x", start[i]);    //line:data:show_bytes_printf
     printf("\n");
 }
 
@@ -40,23 +40,23 @@ void test_show_bytes(int val) {
 /* $end test-show-bytes */
 
 void simple_show_a() {
-/* $begin simple-show-a */
-int val = 0x87654321;
-byte_pointer valp = (byte_pointer) &val;
-show_bytes(valp, 1); /* A. */
-show_bytes(valp, 2); /* B. */
-show_bytes(valp, 3); /* C. */
-/* $end simple-show-a */
+    /* $begin simple-show-a */
+    int val = 0x87654321;
+    byte_pointer valp = (byte_pointer) &val;
+    show_bytes(valp, 1); /* A. */
+    show_bytes(valp, 2); /* B. */
+    show_bytes(valp, 3); /* C. */
+    /* $end simple-show-a */
 }
 
 void simple_show_b() {
-/* $begin simple-show-b */
-int val = 0x12345678;
-byte_pointer valp = (byte_pointer) &val;
-show_bytes(valp, 1); /* A. */
-show_bytes(valp, 2); /* B. */
-show_bytes(valp, 3); /* C. */
-/* $end simple-show-b */
+    /* $begin simple-show-b */
+    int val = 0x12345678;
+    byte_pointer valp = (byte_pointer) &val;
+    show_bytes(valp, 1); /* A. */
+    show_bytes(valp, 2); /* B. */
+    show_bytes(valp, 3); /* C. */
+    /* $end simple-show-b */
 }
 
 void float_eg() {
@@ -75,17 +75,17 @@ void float_eg() {
 }
 
 void string_ueg() {
-/* $begin show-ustring */
-const char *s = "ABCDEF";
-show_bytes((byte_pointer) s, strlen(s)); 
-/* $end show-ustring */
+    /* $begin show-ustring */
+    const char *s = "ABCDEF";
+    show_bytes((byte_pointer) s, strlen(s)); 
+    /* $end show-ustring */
 }
 
 void string_leg() {
-/* $begin show-lstring */
-const char *s = "abcdef";
-show_bytes((byte_pointer) s, strlen(s)); 
-/* $end show-lstring */
+    /* $begin show-lstring */
+    const char *s = "abcdef";
+    show_bytes((byte_pointer) s, strlen(s)); 
+    /* $end show-lstring */
 }
 
 void show_twocomp() 
@@ -104,24 +104,24 @@ int main(int argc, char *argv[])
     int val = 12345;
 
     if (argc > 1) {
-	if (argc > 1) {
-	    val = strtol(argv[1], NULL, 0);
-	}
-	printf("calling test_show_bytes\n");
-	test_show_bytes(val);
+        if (argc > 1) {
+            val = strtol(argv[1], NULL, 0);
+        }
+        printf("calling test_show_bytes\n");
+        test_show_bytes(val);
     } else {
-	printf("calling show_twocomp\n");
-	show_twocomp();
-	printf("Calling simple_show_a\n");
-	simple_show_a();
-	printf("Calling simple_show_b\n");
-	simple_show_b();
-	printf("Calling float_eg\n");
-	float_eg();
-	printf("Calling string_ueg\n");
-	string_ueg();
-	printf("Calling string_leg\n");
-	string_leg();
+        printf("calling show_twocomp\n");
+        show_twocomp();
+        printf("Calling simple_show_a\n");
+        simple_show_a();
+        printf("Calling simple_show_b\n");
+        simple_show_b();
+        printf("Calling float_eg\n");
+        float_eg();
+        printf("Calling string_ueg\n");
+        string_ueg();
+        printf("Calling string_leg\n");
+        string_leg();
     }
     return 0;
 }
